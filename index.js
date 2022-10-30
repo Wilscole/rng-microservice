bea81150fce22dc7fe68aa094231404bc8fa4180
Cole Wilson <cwilson.cdw@gmail.com>
2022-10-30 18:57:06 -0400

    commit

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 // Add Express
 const express = require("express");
+var cors = require('cors')
 
 // Initialize Express
 const app = express();
 
+var corsOptions = {
+    origin: 'http://localhost:8000',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+
+}
+
 // Create GET request
-app.get("/", (req, res) => {
+app.get("/", cors(corsOptions), (req, res) => {
     let rng = Math.floor(Math.random() * 101);
     res.json(rng);
 });
